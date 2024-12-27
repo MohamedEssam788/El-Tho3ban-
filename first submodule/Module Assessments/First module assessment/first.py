cdc49f71e7285eb47b2ad9ca0861a48898344c99
Mohamed Essam <mohamedeassam788@gmail.com>
2024-12-27 13:33:54 +0200

    first sub module

---
+++ first submodule/Module Assessments/First module assessment/first.py
@@ -0,0 +1,7 @@
+name = "Mohamed Essam"
+age = 24
+project_purpose = '''
+Aims to interact with python built-in function print()
+by introducing myself 
+'''
+print(f'Hi there ,the following is an intro to me and the project.\nMy name is :{name} ,\nage:{age} ,\nand the project puropose: {project_purpose}')
